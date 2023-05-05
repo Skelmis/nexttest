8792d7e1326140af5b709733bf812925a5c38c3c
skelmis <ethan@koldfusion.xyz>
2023-05-05 18:09:23 +1200

    feat: disnake#1018: test pr

--- nexttest/test_disnake/test_1018.py
+++ nexttest/test_disnake/test_1018.py
@@ -8,14 +8,28 @@ class Cog(commands.Cog):
         pass
 
 
+class CogTwo(commands.Cog):
+    async def bot_check(self, ctx) -> bool:
+        pass
+
+
+class CogThree(commands.Cog):
+    async def bot_check_once(self, ctx) -> bool:
+        pass
+
+
 def test_commands_bot():
     bot = commands.Bot(command_prefix="?")
     bot.add_cog(Cog())
+    bot.add_cog(CogTwo())
+    bot.add_cog(CogThree())
 
 
 def test_auto_sharded_bot():
     bot = commands.AutoShardedBot(command_prefix="?")
     bot.add_cog(Cog())
+    bot.add_cog(CogTwo())
+    bot.add_cog(CogThree())
 
 
 def test_inter_bot():
@@ -23,8 +37,20 @@ def test_inter_bot():
     with pytest.raises(TypeError):
         bot.add_cog(Cog())
 
+    with pytest.raises(TypeError):
+        bot.add_cog(CogTwo())
+
+    with pytest.raises(TypeError):
+        bot.add_cog(CogThree())
+
 
 def test_auto_inter_bot():
     bot = commands.AutoShardedInteractionBot()
     with pytest.raises(TypeError):
         bot.add_cog(Cog())
+
+    with pytest.raises(TypeError):
+        bot.add_cog(CogTwo())
+
+    with pytest.raises(TypeError):
+        bot.add_cog(CogThree())
